6ad546595665cbaaf9d2f1691997a6d9449533cd
dmakeroam <cpfapp@gmail.com>
2021-07-12 01:28:58 +0700

    Added chapter 4

--- chapter-4/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "topic-us-east_id" {
-    description = "Topic US East ID"
-    value = aws_sns_topic.topic-us-east.arn
-}
-
-output "topic-topic-us-west_id" {
-     description = "Topic US West ID"
-    value = aws_sns_topic.topic-us-west.arn
-}
