84cfabb5d87eb3d3180bcb5c9ca0649eb1ce6457
dmakeroam <cpfapp@gmail.com>
2021-07-13 00:51:06 +0700

    add: chapter 8

---
+++ chapter-8/terraform-cli/main.tf
@@ -0,0 +1,51 @@
+#Create and bootstrap webserver
+resource "aws_instance" "webserver" {
+  ami                         = data.aws_ssm_parameter.webserver-ami.value
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.webserver-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd && sudo systemctl start httpd",
+      "echo '<h1><center>My Website via Terraform Version 2</center></h1>' > index.html",
+      "sudo mv index.html /var/www/html/"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+  tags = {
+    Name = "webserver"
+  }
+}
+
+#Create and bootstrap webserver2
+resource "aws_instance" "webserver2" {
+  ami                         = data.aws_ssm_parameter.webserver-ami.value
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.webserver-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd && sudo systemctl start httpd",
+      "echo '<h1><center>My Website via Terraform Version 1</center></h1>' > index.html",
+      "sudo mv index.html /var/www/html/"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+  tags = {
+    Name = "ApacheWebServer"
+  }
+}
